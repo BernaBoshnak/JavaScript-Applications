acb55bebe7ab844a77d0a1ea194aa95d3de1c726
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-06-21 23:28:09 +0300

    added navigation bar

--- 10. Exams/Exam Preparation - Meme Lounge/src/app.js
+++ 10. Exams/Exam Preparation - Meme Lounge/src/app.js
@@ -4,6 +4,7 @@ import { render } from '../node_modules/lit-html/lit-html.js';
 import { homePage } from './views/home.js';
 
 const main = document.querySelector('main');
+setUserNav();
 
 page('/', decorateContext, homePage);
 
@@ -12,6 +13,19 @@ page.start();
 
 function decorateContext(ctx, next) {
     ctx.render = (content) => render(content, main);
+    ctx.setUserNav = setUserNav;
 
     next();
+}
+
+function setUserNav() {
+    const token = sessionStorage.getItem('authToken');
+
+    if (token != null) {
+        document.querySelector('.user').style.display = '';
+        document.querySelector('.guest').style.display = 'none';
+    } else {
+        document.querySelector('.user').style.display = 'none';
+        document.querySelector('.guest').style.display = '';
+    }
 }
