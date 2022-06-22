d16eb0ae42bdf65e7a375c566ef1426f430deff5
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-06-22 22:12:56 +0300

    added login template

--- 10. Exams/Exam Preparation 2 - Cars Tube/src/app.js
+++ 10. Exams/Exam Preparation 2 - Cars Tube/src/app.js
@@ -3,12 +3,14 @@ import { render } from '../node_modules/lit-html/lit-html.js';
 
 import { homePage } from './views/home.js';
 import { registerPage } from './views/register.js';
+import { loginPage } from './views/login.js';
 
 const main = document.getElementById('site-content');
 setUserNav();
 
 page('/', decorateContext, homePage);
 page('/register', decorateContext, registerPage);
+page('/login', decorateContext, loginPage);
 
 page.start();
 
@@ -19,11 +21,12 @@ function decorateContext(ctx, next) {
 }
 
 function setUserNav() {
-    const token = sessionStorage.getItem('userId');
+    const username = sessionStorage.getItem('username');
 
-    if (token != null) {
+    if (username != null) {
         document.getElementById('guest').style.display = 'none';
         document.getElementById('profile').style.display = 'block';
+        document.getElementById('user-greeting').textContent = `Welcome, ${username}`;
     } else {
         document.getElementById('guest').style.display = 'block';
         document.getElementById('profile').style.display = 'none';
