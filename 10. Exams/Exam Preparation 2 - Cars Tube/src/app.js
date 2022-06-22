32c5754e61b788748e7c1708438eb6eb0ea1dcf2
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-06-22 22:20:08 +0300

    added functionality to logout of account

--- 10. Exams/Exam Preparation 2 - Cars Tube/src/app.js
+++ 10. Exams/Exam Preparation 2 - Cars Tube/src/app.js
@@ -1,18 +1,29 @@
 import page from '../node_modules/page/page.mjs';
 import { render } from '../node_modules/lit-html/lit-html.js';
 
+import { logout as apiLogout } from './api/data.js';
 import { homePage } from './views/home.js';
 import { registerPage } from './views/register.js';
 import { loginPage } from './views/login.js';
 
 const main = document.getElementById('site-content');
+document.getElementById('logoutBtn').addEventListener('click', logout);
 setUserNav();
 
-page('/', decorateContext, homePage);
+page('/', decorateContext, guestUserOnly, homePage);
 page('/register', decorateContext, registerPage);
 page('/login', decorateContext, loginPage);
 
 page.start();
+
+function guestUserOnly(ctx, next) {
+    const token = sessionStorage.getItem('authToken');
+    if (token != null) {
+        return ctx.page.redirect('/catalog');
+    }
+
+    next();
+}
 
 function decorateContext(ctx, next) {
     ctx.render = (content) => render(content, main);
@@ -31,4 +42,10 @@ function setUserNav() {
         document.getElementById('guest').style.display = 'block';
         document.getElementById('profile').style.display = 'none';
     }
+}
+
+async function logout() {
+    await apiLogout();
+    setUserNav();
+    page.redirect('/');
 }
