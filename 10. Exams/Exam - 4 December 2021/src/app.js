46962fbbbb02005285d26e88963497c7be1f762e
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-06-23 14:21:20 +0300

    added navigation bar

--- 10. Exams/Exam - 4 December 2021/src/app.js
+++ 10. Exams/Exam - 4 December 2021/src/app.js
@@ -4,6 +4,7 @@ import { render } from '../node_modules/lit-html/lit-html.js';
 import { homePage } from './views/home.js';
 
 const main = document.getElementById('main-content');
+setUserNav();
 
 page('/', decorateContext, homePage);
 
@@ -11,6 +12,19 @@ page.start();
 
 function decorateContext(ctx, next) {
     ctx.render = (content) => render(content, main);
+    ctx.setUserNav = setUserNav;
 
     next();
+}
+
+function setUserNav() {
+    const email = sessionStorage.getItem('email');
+
+    if (email != null) {
+        document.getElementById('guest').style.display = 'none';
+        document.getElementById('user').style.display = 'inline-block';
+    } else {
+        document.getElementById('guest').style.display = 'inline-block';
+        document.getElementById('user').style.display = 'none';
+    }
 }
