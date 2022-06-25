46bb99088561a51aa262e25757d3ccb2a62d8f99
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-06-25 18:10:08 +0300

    added home template

---
+++ 10. Exams/Retake Exam - 16 Aug 2021/src/app.js
@@ -0,0 +1,16 @@
+import page from '../node_modules/page/page.mjs';
+import { render } from '../node_modules/lit-html/lit-html.js';
+
+import { homePage } from './views/home.js';
+
+const main = document.getElementById('main-content');
+
+page('/', decorateContext, homePage);
+
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content,main);
+
+    next();
+}
