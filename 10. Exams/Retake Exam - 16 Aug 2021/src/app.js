90495a542e80cbad7c888e806cae97deee3503bc
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-06-25 18:18:16 +0300

    added navigation bar

--- 10. Exams/Retake Exam - 16 Aug 2021/src/app.js
+++ 10. Exams/Retake Exam - 16 Aug 2021/src/app.js
@@ -1,16 +1,31 @@
 import page from '../node_modules/page/page.mjs';
 import { render } from '../node_modules/lit-html/lit-html.js';
 
+import { getUserData } from './utility.js';
 import { homePage } from './views/home.js';
 
 const main = document.getElementById('main-content');
+setUserNav();
 
 page('/', decorateContext, homePage);
 
 page.start();
 
 function decorateContext(ctx, next) {
-    ctx.render = (content) => render(content,main);
+    ctx.render = (content) => render(content, main);
+    ctx.setUserNav = setUserNav;
+    ctx.user = getUserData();
 
     next();
+}
+
+function setUserNav() {
+    const user = getUserData();
+    if (user) {
+        document.getElementById('user').style.display = 'block';
+        document.getElementById('guest').style.display = 'none';
+    } else {
+        document.getElementById('user').style.display = 'none';
+        document.getElementById('guest').style.display = 'block';
+    }
 }
